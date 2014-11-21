ca40e93081318e55eac9b5c4332eb0c5c1127a58
TinaLoh <tinalola@gmail.com>
2014-11-21 07:12:01 -0800

    second commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+
+  def home
+   @quotes = [
+     ['"Failure is not an option.  Everyone has to Succeed"', 'Arnold Schwazenegger'] ,
+     ['"Your time is limited, so don\'t waste it living someone else\'s life"', 'Steve Jobs'] ,
+     ['"Better Ingredients, Better Pizza"', 'Papa John'] ,
+   ]
+
+  end
+
+
 end
