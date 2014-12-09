1a37202cc5efd3a0beb827e1bf055a9e23b9d352
TinaLoh <tinalola@gmail.com>
2014-12-09 07:34:19 -0800

    users can update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,24 @@ class UsersController < ApplicationController
 
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def show
+    # @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_path, notice: "User was updated."
+  end
+
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to root_path, notice: "User was deleted."
+  # end
 
   private
 
