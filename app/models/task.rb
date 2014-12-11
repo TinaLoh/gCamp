151dbba2e35b0fe89f97ffb15dda1f3ef0d3c813
TinaLoh <tinalola@gmail.com>
2014-12-11 12:22:49 -0800

    users cannot enter a past date when create new task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
+  validate  :due_date_cannot_be_in_the_past
 
+  def due_date_cannot_be_in_the_past
+      errors.add(:due_date, "can't be in the past") if
+      !due_date.blank? and due_date < Date.today.to_s
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true 
+  validates :email, uniqueness: true
+  
 
 end
